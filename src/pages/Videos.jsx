31eba0abb1556568fe99cb99eb2daf97a4a06bc8
samkosal <67755908+samkosal@users.noreply.github.com>
2026-05-14 10:57:10 -0700

    list out the files

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,17 +1,21 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getVideos } from '../mockApi.js';
 
 export default function Videos() {
+  const [videos, setVideos] = useState([]);
+
   useEffect(() => {
-    getVideos().then((data) => {
-      console.log("getVideos returned:", data);
-    });
+    getVideos().then((data) => setVideos(data));
   }, []);
 
   return (
     <div>
       <h1>Available Videos</h1>
-      <p>Video list will go here.</p>
+      <ul>
+        {videos.map((filename) => (
+          <li key={filename}>{filename}</li>
+        ))}
+      </ul>
     </div>
   );
 }
