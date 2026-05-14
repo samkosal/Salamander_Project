5195c6a9530d2f133823a138a627351e71293561
Omnissiah <234093135+Zeta1313@users.noreply.github.com>
2026-05-14 11:15:50 -0700

    Undid manual lengthening for testing'

--- src/mockApi.js
+++ src/mockApi.js
@@ -22,7 +22,7 @@ const thumbnails = {
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export async function getVideos() {
-  await delay(2000);
+  await delay(400);
   return videos;
 }
 
