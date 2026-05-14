88a0971fb5c718b5b5c2b0cd9a54fb26cc25e666
samkosal <67755908+samkosal@users.noreply.github.com>
2026-05-14 10:24:24 -0700

    added pages with home and video

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Salamander Tracker</h1>
+      <p>Pick a video from the Videos page to start analyzing.</p>
+    </div>
+  );
+}
---
+++ src/pages/Videos.jsx
@@ -0,0 +1,8 @@
+export default function Videos() {
+  return (
+    <div>
+      <h1>Available Videos</h1>
+      <p>Video list will go here.</p>
+    </div>
+  );
+}
