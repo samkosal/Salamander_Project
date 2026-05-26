406432571c8e60309ccf2a6123285fbed054a506
samkosal <67755908+samkosal@users.noreply.github.com>
2026-05-26 11:44:08 -0700

    added two input, color, range

--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -1,15 +1,21 @@
 import { Link, useParams } from 'react-router-dom';
 import { getThumbnail } from '../mockApi.js';
-import { useEffect, useState } from 'react'
+import { useRef, useState, useEffect } from 'react';
 
 export default function Preview() {
     const { filename } = useParams();
 
+    //ref
+    const canvasRef = useRef(null);
+    const imgRef = useRef(null);
+
+    //state
     const [thumbnail, setThumbnail] = useState([]);
-
     const [loading, setLoading] = useState(true);
-
     const [error, setError] = useState(null);
+    const [imageReady, setImageReady] = useState(false);
+    const [color, setColor] = useState('#ffffff');
+    const [tolerance, setTolerance] = useState(0);
 
     useEffect(() => {
         getThumbnail(filename)
@@ -35,6 +41,9 @@ export default function Preview() {
         <div className="min-h-screen bg-black text-green-600 grid place-items-center px-50 font-mono">
         <h1>Preview: {filename}</h1>
         <img src = {thumbnail} alt = {thumbnail} />
+        <input type="color" onChange={(e) => setColor(e.target.value)} value={color} />
+        <input type="range" onChange={(e) => setTolerance(e.target.value)} value={tolerance} />
+        <canvas className="bg-white border-2" ref={canvasRef} />
         <p>Tuning controls will go here in a future pair program.</p>
         <Link to="/videos" className="hover:text-red-600">Back to videos</Link>
         </div>
